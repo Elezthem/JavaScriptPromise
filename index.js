61200147bb6c485e5a519d766125e3f366798ea9
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-10-02 19:41:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const getData = (url) =>
+    new Promise((resolve, reject) =>
+      fetch(url)
+        .then(response => response.json())
+        .then(json => resolve(json))
+        .catch(error => reject(error))
+    )
+
+
+getData('https://jsonplaceholder.typicode.com/todos')
+    .then(data => console.log(data))
+    .catch(error => console.log(error.message))
